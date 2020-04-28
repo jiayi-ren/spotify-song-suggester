a89e46541124e8665e8c01a5066408b67f6b99f9
Jiayi Ren <jiayiren22@gmail.com>
2020-04-27 22:49:55 -0700

    fix login modal close bug

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,10 +3,12 @@ import Login from './Login.js';
 
 const Modal = props =>{
 
+    const {spanClick} = props
+
     return(
         <div className="modal">
             <div className="modal-content">
-                <span className="modal-close">&times;</span>
+                <span className="modal-close" onClick={spanClick}>&times;</span>
                 <Login />
             </div>
         </div>
