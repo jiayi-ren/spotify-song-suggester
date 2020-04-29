b62242b0377088db8f4c1569d3c0a458530032b8
Jiayi Ren <jiayiren22@gmail.com>
2020-04-29 08:34:17 -0700

    update SongCard, app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./components/protected/ProtectedRoute";
 import './App.css';
-import Nav from './components/Nav.js';
+
 import HomePage from './components/HomePage.js';
-import MusicPage from './components/MusicPage';
-import UserPage from './components/UserPage.js';
 import Login from './components/Login.js';
 import SignUp from './components/SignUp';
-import DummyPage from './components/DummyPage';
-import Callback from './components/Callback';
-
+import Callback from './components/protected/Callback';
+import SearchList from './components/newFiles/SearchList';
+import SavedList from './components/newFiles/SavedList';
 
 
 function App() {
@@ -33,22 +31,27 @@ function App() {
         })
       } 
       <Router>
-          <Nav />
+          <nav>
+            <NavLink to="/">
+              <h3 className="navLink">Home</h3>
+            </NavLink>
+            <NavLink to="/login">
+              <h3 className="navLink">Login</h3>
+            </NavLink>
+            <NavLink to="/music">
+              <h3 className="navLink">Search</h3>
+            </NavLink>
+            <NavLink to="/dashboard">
+              <h3 className="navLink">Saved</h3>
+            </NavLink>
+          </nav>
+          
           <Route exact path="/" component={HomePage} />
-          <Route path="/callback" component={Callback} />
-          <ProtectedRoute exact path="/Dummy" component={DummyPage} />
-          <Route exact path="/login">
-            <div className="login-page">
-                <Login />
-                <SignUp />
-            </div>
-            </Route>
-          <Route exact path="/music">
-            <MusicPage addToSavedList={addToSavedList}/>  
-          </Route>
-          <Route exact path="/dashboard">
-            <UserPage savedList={savedList}/>
-          </Route>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/sign-up" component={SignUp} />
+          <Route path="/callback/:token" component={Callback} />
+          <ProtectedRoute exact path="/music" component={SearchList} />
+          <ProtectedRoute exact path="/dashboard" component={SavedList} />
 
       </Router>
     </div>
