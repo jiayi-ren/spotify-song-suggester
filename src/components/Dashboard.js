9a308dd10832b8e41d582939b7e7ad5f8497a57d
Jiayi Ren <jiayiren22@gmail.com>
2020-04-30 08:28:22 -0700

    rename searchlist to musicpage, savedlist to dashboard. Add styles on login page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import SongCard from "./SongCard";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const SavedList = props => {
+const Dashboard = props => {
 
     const [savedSongs, setSavedSongs] = useState([]);
 
@@ -20,17 +20,17 @@ const SavedList = props => {
     }, [])
 
     return (
-        <>
-        <h2>Saved Songs</h2>
-        {savedSongs && savedSongs.map((song,index) => {
-            return (
-                <div key={index}>
-                <SongCard song={song} savedSongs={savedSongs} setSavedSongs={setSavedSongs} />
-                </div>
-            )
-        })}
-        </>
+        <div className="dashboard">
+            <h2>My favorites</h2>
+            {savedSongs && savedSongs.map((song,index) => {
+                return (
+                    <div key={index}>
+                    <SongCard song={song} savedSongs={savedSongs} setSavedSongs={setSavedSongs} />
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
-export default SavedList;
+export default Dashboard;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,24 @@
 import React from 'react';
-import icon from '../assests/images/Spotify_Icon_RGB_Green.png';
+import styled from 'styled-components';
+import icon from '../assests/images/Spotify_Icon_RGB_White.png';
 import SignUp from './SignUp';
+
+const StyledButton = styled.button`
+    width: 320px;
+    padding:15px 45px;
+    border-radius:500px;
+    background-color:#1DB954;
+    transition: all 0.3s ease-in-out;
+    &:hover {
+        background-color: #1ed760;
+        transition: all 0.3s ease-in-out;
+    }
+`
+
+const StyledLink = styled.a`
+    color:#FFFFFF;
+    font-size: 18px;
+`
 
 const url = 'http://localhost:3000/callback/'
 
@@ -9,20 +27,27 @@ const Login = props =>{
     const loginLink = `https://spotify-song-api.herokuapp.com/api/auth/spotify?redirect_url=${url}`
 
     return(
-        <div className="login">
-            <h3>Already have a Spotify Account?</h3>
-            <button className="sign-in-btn">
-                <a href={loginLink}>
-                    Sign in with Spotify&nbsp;
-                    <img src={icon} className="icon" alt="Spotify icon" />
-                </a>
-            </button>
-
-            <h3>Don't have a Spotify account yet?</h3>
-            <button className="sign-in-btn">
-                <a href='https://www.spotify.com/us/signup/'>Sign up Here!</a>
-            </button>
-            <SignUp />
+        <div className="login-container">
+            <div className="login">
+                <div className="spotify-login">
+                    <div>
+                    <h3>Already have a Spotify Account?</h3>
+                    <StyledButton className="sign-in-btn">
+                        <StyledLink href={loginLink}>
+                            Log in with Spotify&nbsp;
+                            <img src={icon} className="icon" alt="Spotify icon" />
+                        </StyledLink>
+                    </StyledButton>
+                    <h3>Don't have a Spotify account yet?</h3>
+                    <StyledButton className="sign-in-btn">
+                        <StyledLink href='https://www.spotify.com/us/signup/'>Sign Up Here!</StyledLink>
+                    </StyledButton>
+                    </div>
+                </div>
+                <div className="app-login">
+                    <SignUp />
+                </div>
+            </div>
         </div>
     )
 }
